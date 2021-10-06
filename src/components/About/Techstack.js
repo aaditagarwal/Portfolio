9050c4fe68c63b1f3ad213bae011f7a5e45e6fe6
Aadit Agarwal <agarwal.aadit99@gmail.com>
2021-10-07 01:32:42 +0530

    TechStack hover update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -18,33 +18,83 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <div className="overlay">
+          <div className="fadeText">
+            Python
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
+        <div className="overlay">
+          <div className="fadeText">
+            C++
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMysql />
+        <div className="overlay">
+          <div className="fadeText">
+            MySQL
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPostgresql  />
+        <div className="overlay">
+          <div className="fadeText">
+            PostgreSQL
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <div className="overlay">
+          <div className="fadeText">
+            JavaScript
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <div className="overlay">
+          <div className="fadeText">
+            ReactJS
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <div className="overlay">
+          <div className="fadeText">
+            NodeJS
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiHtml5 />
+        <div className="overlay">
+          <div className="fadeText">
+            HTML
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3 />
+        <div className="overlay">
+          <div className="fadeText">
+            CSS
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <div className="overlay">
+          <div className="fadeText">
+            Git
+          </div>
+        </div>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -13,18 +13,43 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+        <div className="overlay">
+          <div className="fadeText">
+            Linux
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGithubBadge />
+        <div className="overlay">
+          <div className="fadeText">
+            GitHub
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <div className="overlay">
+          <div className="fadeText">
+            VS Code
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJupyter />
+        <div className="overlay">
+          <div className="fadeText">
+            Jupyter
+          </div>
+        </div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <div className="overlay">
+          <div className="fadeText">
+            Postman
+          </div>
+        </div>
       </Col>
     </Row>
   );
