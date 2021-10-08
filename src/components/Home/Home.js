128a52b16ca2c5033b8bc6cfb2ed482e4a736d2c
aaditagarwal <agarwal.aadit99@gmail.com>
2021-10-09 02:28:53 +0530

    Home Page Layout Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,135 +1,70 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.png"
 import Particle from "../Particle";
-import Home2 from "./Home2";
-import laptopImg from "../../Assets/about.png"
+import { Container, Row, Col } from "react-bootstrap";
 import Type from "./Type";
-import Tilt from "react-parallax-tilt";
-import {
-  AiFillMail,
-  AiFillGithub,
-  AiFillInstagram,
-  AiOutlineGoogle
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import Home2 from "./Home2";
+import avatar from "../../Assets/avatar.png"
+import laptopImg from "../../Assets/about.png"
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
+        <Container>
+          <Particle />
+          <Container className="home-content">
+            <Row style={{ justifyContent: "center", padding: "10px" }}>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Hello There! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+                </h1>
+
+                <h1 className="heading-name">
+                  I'M
+                  <strong className="main-name"> AADIT AGARWAL</strong>
+                </h1>
+
+                <h3 className="subheading-name">
+                  Welcome to my corner on the internet.
+                </h3>
+
+                <div className="home-typeWritter">
+                  <Type />
+                </div>
+              </Col>
+
+              <Col md={4} className="myAvtar">
+                <img src={avatar} className="img-fluid" alt="avatar" />
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      
         <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hello There! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+        <Container>
+          <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <Col
+              md={7}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+            >
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                <strong className="purple"> Know Who I'M</strong>
               </h1>
-
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> AADIT AGARWAL</strong>
-              </h1>
-
-              <h3 className="subheading-name">
-                Welcome to my corner on the internet.
-              </h3>
-
-              <div style={{ padding: 95, textAlign: "left" }}>
-                <Type />
-              </div>
+              <Home2 />
             </Col>
-
-            <Col md={5} className="myAvtar-home">
-              <Tilt>
-                <img src={myImg} className="img-fluid" alt="avatar" />
-              </Tilt>
+            <Col
+              md={5}
+              style={{ paddingTop: "120px", paddingBottom: "50px" }}
+              className="about-img"
+            >
+              <img src={laptopImg} alt="about" className="img-fluid" />
             </Col>
           </Row>
         </Container>
-      </Container>
-      
-      <Particle />
-      <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-        <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              <strong className="purple"> Know Who I'M</strong>
-            </h1>
-            <Home2 />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="mailto:agarwal.aadit99@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillMail />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/aaditagarwal/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://github.com/aaditagarwal"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://scholar.google.com/citations?hl=en&user=2EOgF5oAAAAJ"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                <AiOutlineGoogle />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/aaditagarwal/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
       </Container>
     </section>
   );
